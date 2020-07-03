f1376c93738f1386ae77c6f3c4825374b0780455
windowpi <justin.ludwick17@gmail.com>
2020-07-03 15:22:44 -0500

    booty

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def main():
 def about():
     return render_template('about.html')
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)
+#if __name__ == "__main__":
+#    app.run(debug=True, host="0.0.0.0", port=80)
