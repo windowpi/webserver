79948ea316e1a583641f485fae7ad8e11e8a6835
windowpi <justin.ludwick17@gmail.com>
2020-07-03 14:36:26 -0500

    poop

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return("Welcome to my Flask page!")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
